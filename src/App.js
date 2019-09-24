b463fa3e870493404cb49e73824610e592c094d6
Will Schulz <will.schulz29@gmail.com>
2019-09-23 23:17:03 -0400

    photo of the day with env vars

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [imageAlt, setImageAlt] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    async function fetchPhoto() {
+      let url = "https://api.nasa.gov/planetary/apod";
+      let config = {
+        params: { api_key: process.env.REACT_APP_NASAKEY }
+      };
+
+      const res = await axios(url, config);
+      setImageAlt(res.data.title);
+      setImageSrc(res.data.url);
+    }
+    fetchPhoto();
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa Photo of the Day</h1>
+      <img alt={imageAlt} src={imageSrc}></img>
     </div>
   );
 }
